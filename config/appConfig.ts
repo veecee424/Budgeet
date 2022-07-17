f9a2bb3930905ecd6e6ff20283c5d2a3e2eb5f2e
veecee424 <valentine424@yahoo.com>
2022-07-17 23:29:32 +0100

    modify db config

--- config/appConfig.ts
+++ config/appConfig.ts
@@ -1,3 +1,4 @@
 export const config = {
-    jwtSecret: process.env.JWT_SECRET || '3ROYW1&^lIjoiVmV%@'
+    jwtSecret: process.env.JWT_SECRET || '3ROYW1&^lIjoiVmV%@',
+    useLocalDB: process.env.LOCAL_DB || 1
 } 
